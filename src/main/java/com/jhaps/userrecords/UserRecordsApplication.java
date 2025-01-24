83596db5a23756d2467416d95cd976319fb3757a
MR_JHAPS <nerazole@gmail.com>
2025-01-24 21:04:21 +0100

    first commit

---
+++ src/main/java/com/jhaps/userrecords/UserRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.jhaps.userrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhaps/userrecords/UserRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhaps.userrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
